1574255db231d6f718197e5607174ada7e3d45be
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-09-24 17:12:07 +0300

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super().__init__()
+
+        # YOUR CODE HERE
+        # Define a CNN architecture. Remember to use the variable num_classes
+        # to size appropriately the output of your classifier, and if you use
+        # the Dropout layer, use the variable "dropout" to indicate how much
+        # to use (like nn.Dropout(p=dropout))
+        
+        # Conv
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv6 = nn.Conv2d(256, 512, 3, padding=1)
+        
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.bn6 = nn.BatchNorm2d(512)
+        
+        self.relu = nn.ReLU()
+        self.mp = nn.MaxPool2d(2, 2)
+        
+        
+        # Dense
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(4608, 500),
+            nn.Dropout(dropout),
+            nn.Linear(500, num_classes)
+        )
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # YOUR CODE HERE: process the input tensor through the
+        # feature extractor, the pooling and the final linear
+        # layers (if appropriate for the architecture chosen)
+        
+        x = self.mp(self.bn1(self.relu(self.conv1(x))))
+        x = self.mp(self.bn2(self.relu(self.conv2(x))))
+        x = self.mp(self.bn3(self.relu(self.conv3(x))))
+        x = self.mp(self.bn4(self.relu(self.conv4(x))))
+        x = self.mp(self.bn5(self.relu(self.conv5(x))))
+        x = self.mp(self.bn6(self.relu(self.conv6(x))))
+        x = self.fc(x)
+        
+        return x
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = dataiter.next()
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
